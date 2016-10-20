9b5d5d51c23e7e41bb8b2cae3bfd346d61b686f8
setevoy2 <arseny.zinchenko@akqa.com>
2016-10-20 13:48:36 +0300

    1

--- docker_code.build.groovy
+++ docker_code.build.groovy
@@ -13,27 +13,29 @@ def DOCKER_IMAGE_TAG="${ENVIRONMENT}-${env.BUILD_NUMBER}"
 
 node {
 
-    stage 'Maven package'
+    stage ('Maven package') {
 
-    docker.image('maven').inside {
+        docker.image('maven').inside {
 
-        git branch: "${BRANCH}", credentialsId: 'githubSetevoy', url: "${CODE_REPO}"
+            git branch: "${BRANCH}", credentialsId: 'githubSetevoy', url: "${CODE_REPO}"
 
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${DOCKER_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${DOCKER_CREDENTIALS_ID}", 
+            usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 
-            sh 'echo uname=$USERNAME pwd=$PASSWORD'
+                sh 'echo uname=$USERNAME pwd=$PASSWORD'
+   
+                sh 'mvn clean install -DskipDockerBuild'
 
-            sh 'mvn clean install -DskipDockerBuild'
-
-            sh "mvn clean package \
-              -s./maven-settings.xml \
-              -Ddocker.registry.id=${DOCKER_REGISTRY_ID} \
-              -Ddocker.registry.host=${DOCKER_REGISTRY_HOST} \
-              -Ddocker.registry.url=${DOCKER_REGISTRY_URL} \
-              -Ddocker.registry.username=${DOCKER_REGISTRY_USERNAME} \
-              -Ddocker.registry.password=${DOCKER_REGISTRY_PASSWORD} \
-              -Ddocker.image.tag=\"${DOCKER_IMAGE_TAG}\" \
-              -DpushImage"
+                sh "mvn clean package \
+                  -s./maven-settings.xml \
+                  -Ddocker.registry.id=${DOCKER_REGISTRY_ID} \
+                  -Ddocker.registry.host=${DOCKER_REGISTRY_HOST} \
+                  -Ddocker.registry.url=${DOCKER_REGISTRY_URL} \
+                  -Ddocker.registry.username=${DOCKER_REGISTRY_USERNAME} \
+                  -Ddocker.registry.password=${DOCKER_REGISTRY_PASSWORD} \
+                  -Ddocker.image.tag=\"${DOCKER_IMAGE_TAG}\" \
+                  -DpushImage"
+            }
         }
     }
 }
