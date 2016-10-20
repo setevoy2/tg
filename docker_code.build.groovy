234f863af22381613e19136a37e18fb3d9dab024
setevoy2 <arseny.zinchenko@akqa.com>
2016-10-20 13:41:21 +0300

    hh

--- docker_code.build.groovy
+++ docker_code.build.groovy
@@ -2,9 +2,12 @@ def CODE_REPO="https://github.com/setevoy2/tg.git"
 def BRANCH="master"
 def ENVIRONMENT="dev"
 
-def DOCKER_REGISTRY_ID="setevoy"
+def DOCKER_CREDENTIALS_ID="setevoyDocHub"
+def DOCKER_REGISTRY_ID=""
 def DOCKER_REGISTRY_URL="https://hub.docker.com/"
 def DOCKER_REGISTRY_HOST="hub.docker.com"
+def DOCKER_REGISTRY_USERNAME=""
+def DOCKER_REGISTRY_PASSWORD=""
 def DOCKER_IMAGE_PATH=""
 def DOCKER_IMAGE_TAG="${ENVIRONMENT}-${env.BUILD_NUMBER}"
 
@@ -16,16 +19,21 @@ node {
 
         git branch: "${BRANCH}", credentialsId: 'githubSetevoy', url: "${CODE_REPO}"
 
-        sh 'mvn clean install -DskipDockerBuild'
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${DOCKER_CREDENTIALS_ID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
 
-        sh "mvn clean package \
-          -s./maven-settings.xml \
-          -Ddocker.registry.id=${DOCKER_REGISTRY_ID} \
-          -Ddocker.registry.host=${DOCKER_REGISTRY_HOST} \
-          -Ddocker.registry.url=${DOCKER_REGISTRY_URL} \
-          -Ddocker.registry.username=${DOCKER_REGISTRY_USERNAME} \
-          -Ddocker.registry.password=${DOCKER_REGISTRY_PASSWORD} \
-          -Ddocker.image.tag=\"${DOCKER_IMAGE_TAG}\" \
-          -DpushImage"
+            sh 'echo uname=$USERNAME pwd=$PASSWORD'
+
+            sh 'mvn clean install -DskipDockerBuild'
+
+            sh "mvn clean package \
+              -s./maven-settings.xml \
+              -Ddocker.registry.id=${DOCKER_REGISTRY_ID} \
+              -Ddocker.registry.host=${DOCKER_REGISTRY_HOST} \
+              -Ddocker.registry.url=${DOCKER_REGISTRY_URL} \
+              -Ddocker.registry.username=${DOCKER_REGISTRY_USERNAME} \
+              -Ddocker.registry.password=${DOCKER_REGISTRY_PASSWORD} \
+              -Ddocker.image.tag=\"${DOCKER_IMAGE_TAG}\" \
+              -DpushImage"
+        }
     }
 }
