e0b6e721f67e7af58ca9617ea938706f38b35cb4
setevoy2 <arseny.zinchenko@akqa.com>
2016-10-20 13:53:24 +0300

    withCredentials test

--- creds.groovy
+++ creds.groovy
@@ -1,8 +1,13 @@
 node {
-  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'setevoyDocHub',
-                    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-    sh '''
-      echo "User: $USERNAME, password: $PASSWORD"
-    '''
-  }
+
+    stage('Creds test') {
+
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'setevoyDocHub',
+        usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+
+            sh '''
+              echo "User: $USERNAME, password: $PASSWORD"
+            '''
+        }
+    }
 }
